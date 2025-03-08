2c5d212fafa060b5e0ef9db872eb2a53d415659f
Rishabh Kumar Das <51358252+rishabhd-cognavi@users.noreply.github.com>
2025-03-09 00:28:08 +0530

    Update TableForSKU.tsx

--- src/components/ui/skus/TableForSKU.tsx
+++ src/components/ui/skus/TableForSKU.tsx
@@ -25,7 +25,7 @@ export default function TableForSKU({ handleOpenDialog, handleDeleteSku }: Table
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
-  const handleChangePage = (_event: unknown, newPage: number) => {
+  const handleChangePage = (_: unknown, newPage: number) => {
     setPage(newPage);
   };
 
