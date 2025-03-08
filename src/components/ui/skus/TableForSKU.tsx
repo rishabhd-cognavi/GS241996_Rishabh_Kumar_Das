4b9bc53b29be138ef4c2bc04e382f549e8f82635
Rishabh Kumar Das <51358252+rishabhd-cognavi@users.noreply.github.com>
2025-03-09 00:34:59 +0530

    Update TableForSKU.tsx

--- src/components/ui/skus/TableForSKU.tsx
+++ src/components/ui/skus/TableForSKU.tsx
@@ -8,12 +8,11 @@ import {
   TableContainer,
   TableHead,
   TableRow,
-  TablePagination,
 } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../store';
 import { SKU } from '../../../models/types';
-import React from 'react';
+// import React from 'react';
 
 interface TableForSKUProps {
   handleOpenDialog: (sku: SKU) => void;
@@ -22,17 +21,17 @@ interface TableForSKUProps {
 
 export default function TableForSKU({ handleOpenDialog, handleDeleteSku }: TableForSKUProps) {
   const skus = useSelector((state: RootState) => state.skus.skus);
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  // const [page, setPage] = React.useState(0);
+  // const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
-  const handleChangePage = (_: unknown, newPage: number) => {
-    setPage(newPage);
-  };
+  // const handleChangePage = (_: unknown, newPage: number) => {
+  //   setPage(newPage);
+  // };
 
-  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
+  // const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setRowsPerPage(parseInt(event.target.value, 10));
+  //   setPage(0);
+  // };
 
   return (
     <TableContainer component={Paper}>
@@ -53,7 +52,7 @@ export default function TableForSKU({ handleOpenDialog, handleDeleteSku }: Table
               </TableCell>
             </TableRow>
           ) : (
-            skus.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((sku) => (
+            skus.map((sku) => (
               <TableRow key={sku.id}>
                 <TableCell>{sku.name}</TableCell>
                 <TableCell align='right'>{formatCurrency(sku.price)}</TableCell>
@@ -71,15 +70,15 @@ export default function TableForSKU({ handleOpenDialog, handleDeleteSku }: Table
           )}
         </TableBody>
       </Table>
-      <TablePagination
+      {/* <TablePagination
         rowsPerPageOptions={[10, 30, 50]}
         component='div'
         count={skus.length}
         rowsPerPage={rowsPerPage}
         page={page}
-        onPageChange={handleChangePage}
+        // onPageChange={handleChangePage}
         onRowsPerPageChange={handleChangeRowsPerPage}
-      />
+      /> */}
     </TableContainer>
   );
 }
